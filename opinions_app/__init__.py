0770942fa5a12d2d10223e323f75d16c0c2ceee6
Denis Miroshnik <kano-kito@yandex.ru>
2025-09-20 18:25:44 +0300

    2009_2

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from settings import Config
 
 app = Flask(__name__)
+app.json.ensure_ascii = False
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import cli_commands, error_handlers, views 
+from . import api_views, cli_commands, error_handlers, views
